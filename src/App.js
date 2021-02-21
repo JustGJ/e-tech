ab705287a00b3d2b434aa725ac5868e68a09c5c6
Jeff <jf.gasp@gmail.com>
2021-02-21 12:10:38 +1100

    add to cart

--- src/App.js
+++ src/App.js
@@ -8,24 +8,40 @@ function App() {
 
   // == On stocke tout nos produits récupérés grâce à fetch
   const [products, setProducts] = useState([]);
+  // == Ajout du panier 
+  const [cart, setCart] = useState({});
 
+  // == On récupère notre boutique commerce.js
   const fetchProducts = async () => {
       const { data } = await commerce.products.list();
 
       setProducts(data);
   }
 
+  // == On récupère notre panier sur commerce.js
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+}
+
+  // == Ajout de l'item dans le panier 
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart)
+    console.log(cart);
+}
   // == On lance fetch (récupération de donnée) une fois le render affiché
   useEffect(() => {
       fetchProducts();
+      fetchCart();
   }, [])
 
+ 
   
   return (
     <div className="App">
-        <Navbar />
+        <Navbar totalItems={cart.total_items} />
         {/* On envoie notre liste de produit récupérée au composant <Products /> */}
-        <Products products={products}/> 
+        <Products products={products} onAddToCart={handleAddToCart} /> 
    </div>
   );
 }
--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -4,12 +4,9 @@ import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
 
     const classes = useStyles();
-
-    console.log(product);
-
 
     return (
         <Card className={classes.root}>
@@ -26,7 +23,7 @@ const Product = ({ product }) => {
                 <Typography dangerouslySetInnerHTML= {{__html: product.description}} variant="body2" color="textSecondary" />
             </CardContent>
             <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label="Add to Cart">
+                <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}> {/* id du produit à envoyer ainsi que la qtité initial */}
                     <AddShoppingCart />
                 </IconButton>
             </CardActions>
